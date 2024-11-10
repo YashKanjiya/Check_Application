cda757a8ff0101f6049bc4f8ab1d84cf327bacf4
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-11-10 10:59:00 +0530

    Add files via upload

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# In[13]:
+
+
+yash=25
+tata=30
+iex=123
+- yash
+- tata
+- iex
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. **NumPy** - for numerical and matrix operations.
+# 2. **Pandas** - for data manipulation and analysis.
+# 3. **Matplotlib** - for data visualization.
+# 4. **Scikit-Learn** - for machine learning and data preprocessing.
+# 5. **TensorFlow** - for deep learning and neural networks.
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[14]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3 * 4) + 5
+
+
+# In[15]:
+
+
+200/6
+
+
+# 
+# - List popular languages for Data Science.
+# - Understand common data science libraries.
+# - Perform basic arithmetic operations in Python.
+# - Convert time units using arithmetic expressions.
+# - Outline essential skills for Data Scientists.
+# 
+
+# Exercise 10 - Insert a markdown cell to list Objectives
+# 
+# Below the introduction cell created in Exercise 3, insert a new markdown cell to list the objectives that this notebook covered (i.e. some of the key takeaways from the course). In this new cell start with an introductory line titled: Objectives: in bold font. Then using an unordered list (bullets) indicate 3 to 5 items covered in this notebook, such as List popular languages for Data Science.
+# Take a screenshot of the markdown cell and name it as 10-objectives.png (Images can be saved with either the .jpg or .png extension.)
+
+# # AUthor
+# Kanjiya Yash
+
+# In[ ]:
+
+
+
+
